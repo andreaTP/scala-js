8b6906a62f09de27fd63be719869e584e69caae8
Andrea <andrea.peruffo1982@gmail.com>
2015-05-05 16:34:17 +0200

    re added abstractset

--- javalib/src/main/scala/java/util/AbstractSet.scala
+++ javalib/src/main/scala/java/util/AbstractSet.scala
@@ -3,44 +3,45 @@ package java.util
 import scala.annotation.tailrec
 
 abstract class AbstractSet[E]() extends java.util.AbstractCollection[E] with java.util.Set[E] {
-	override def equals(o: Any) = {
-		if (o==this) {
-			o match {
-				case s: java.util.Collection[E] => 
-					if (s.size==this.size) {
-						containsAll(s)
-					} else false
-				case _ => false
-			}
-		} else false
-	}
-	override def hashCode(): Int = {
 
-		@tailrec
-		def sumAll(i: Iterator[E], part: Int): Int =
-			if (i.hasNext) sumAll(i, part + i.next.hashCode) 
-			else part
+  override def equals(o: Any): Boolean = {
+    if (o == this) {
+      o match {
+        case s: java.util.Collection[E] =>
+          if (s.size == this.size) {
+            containsAll(s)
+          } else false
+        case _ => false
+      }
+    } else false
+  }
 
-		sumAll(iterator,0)
-	}
-	override final def removeAll(c: java.util.Collection[E]): Boolean = {
+  override def hashCode(): Int = {
+    @tailrec
+    def sumAll(i: Iterator[E], part: Int): Int =
+      if (i.hasNext) sumAll(i, part + i.next.hashCode)
+      else part
 
-		@tailrec
-		def remAll(iter: Iterator[E], comp: java.util.Collection[E], res: Boolean = false): Boolean = {
-			if (iter.hasNext) {
-				val elem = iter.next
-				if (comp.contains(elem)) {
-					remove(elem)
-					remAll(iter, comp, true)
-				} else 
-					remAll(iter, comp, res)
-			} else res
-		}
+    sumAll(iterator, 0)
+  }
 
-		
-		if (size>c.size)
-			remAll(c.iterator, this)
-		else 
-			remAll(iterator, c)
-	}
+  override final def removeAll(c: java.util.Collection[E]): Boolean = {
+
+    @tailrec
+    def remAll(iter: Iterator[E], comp: java.util.Collection[E], res: Boolean = false): Boolean = {
+      if (iter.hasNext) {
+        val elem = iter.next
+        if (comp.contains(elem)) {
+          remove(elem)
+          remAll(iter, comp, true)
+        } else
+          remAll(iter, comp, res)
+      } else res
+    }
+
+    if (size > c.size)
+      remAll(c.iterator, this)
+    else
+      remAll(iterator, c)
+  }
 }
